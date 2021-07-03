3d5029e2d267d8d4ef3cea4c752a94474846c701
CabesaDario <darioojeda_1995@hotmail.com>
2021-07-03 17:58:02 +0200

    peticion http con fetch(provisional)

--- src/app/gifs/services/gifs.service.ts
+++ src/app/gifs/services/gifs.service.ts
@@ -6,13 +6,14 @@ import { Injectable } from '@angular/core';
 //Angular lo eleva a un nivel global
 export class GifsService {
   
+  private apiKey    : string = "uP0RyN5oOVOLQhfCseOKY0fn1jRLdt4i";
   private _historial: string[] = [];
 
   get historial() {
     return [...this._historial];
   }
 
-  buscarGifs ( query: string = '') {
+  async buscarGifs ( query: string = '') {
 
     query = query.trim().toLocaleLowerCase();
 
@@ -20,8 +21,10 @@ export class GifsService {
       this._historial.unshift( query );
       this._historial = this._historial.splice(0,10);
     }
+    const resp = await fetch('https://api.giphy.com/v1/gifs/search?api_key=uP0RyN5oOVOLQhfCseOKY0fn1jRLdt4i&q=dragon ball z&limit=10');
+    const data = await resp.json();
 
-    console.log(this._historial);
+    console.log(data);
   
 
     
