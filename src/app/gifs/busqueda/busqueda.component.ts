01bbfb5f9be2c42f68a50ed5aad04bbcc8562fb2
CabesaDario <darioojeda_1995@hotmail.com>
2021-07-03 17:21:54 +0200

    Gifs services y resultados y busqueda component
    Se ha añadido un servicio para controlar un historial de busqueda y dos componentes en el modulo de gifs: uno que muestra los resultados y otro encargado de la búsqueda.

---
+++ src/app/gifs/busqueda/busqueda.component.ts
@@ -0,0 +1,31 @@
+import { ElementRef, ViewChild, Component } from '@angular/core';
+import { ChildActivationStart } from '@angular/router';
+import { GifsService } from '../services/gifs.service';
+
+@Component({
+  selector: 'app-busqueda',
+  templateUrl: './busqueda.component.html',
+  styles: [
+  ]
+})
+export class BusquedaComponent{
+
+    @ViewChild('txtBuscar') txtBuscar!: ElementRef<HTMLInputElement>;
+  //este operador garantiza al compilador que el objeto no es nulo "Non-null assertion operator"
+  //propio de typescript
+
+    constructor( private gifsService: GifsService ) {}
+
+    buscar( ): void {
+      const valor = this.txtBuscar.nativeElement.value;
+
+      if ( valor.trim().length === 0){
+        return;
+      }
+      
+      this.gifsService.buscarGifs(valor);
+
+      this.txtBuscar.nativeElement.value = '';
+    }
+
+}
